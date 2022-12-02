316656d63a26d531c3b83bf6a2130ba75312abe6
Lukasz Dechnik <lukasz@dechnik.net>
2022-12-02 11:23:16 +0100

    bolek config

---
+++ home/lukasz/global/default.nix
@@ -0,0 +1,47 @@
+{ inputs, lib, pkgs, config, outputs, ... }:
+let
+  inherit (inputs.nix-colors) colorSchemes;
+  inherit (inputs.nix-colors.lib-contrib { inherit pkgs; }) colorschemeFromPicture nixWallpaperFromScheme;
+in
+{
+  imports = [
+    inputs.impermanence.nixosModules.home-manager.impermanence
+    inputs.nix-colors.homeManagerModule
+  ] ++ (builtins.attrValues outputs.homeManagerModules);
+  colorscheme = lib.mkDefault colorSchemes.gruvbox-dark-hard;
+
+  home.file.".colorscheme".text = config.colorscheme.slug;
+
+  systemd.user.startServices = "sd-switch";
+
+  programs = {
+    home-manager.enable = true;
+    git.enable = true;
+  };
+
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      warn-dirty = false;
+    };
+  };
+
+  home = {
+    username = lib.mkDefault "lukasz";
+    homeDirectory = lib.mkDefault "/home/${config.home.username}";
+    stateVersion = lib.mkDefault "22.05";
+
+    persistence = {
+      "/persist/home/lukasz" = {
+        directories = [
+          "Documents"
+          "Downloads"
+          "Pictures"
+          "Videos"
+        ];
+        allowOther = true;
+      };
+    };
+  };
+}
---
+++ hosts/common/global/default.nix
@@ -0,0 +1,49 @@
+{ lib, inputs, outputs, ... }:
+{
+  imports = [
+    inputs.impermanence.nixosModules.impermanence
+    ./fish.nix
+    ./locale.nix
+    ./nix.nix
+    ./openssh.nix
+    ./sops.nix
+  ] ++ (builtins.attrValues outputs.nixosModules);
+
+  networking.domain = "dechnik.net";
+
+  environment = {
+    loginShellInit = ''
+      # Activate home-manager environment, if not already
+      [ -d "$HOME/.nix-profile" ] || /nix/var/nix/profiles/per-user/$USER/home-manager/activate &> /dev/null
+    '';
+
+    # Persist logs, timers, etc
+    persistence = {
+      "/persist".directories = [ "/var/lib/systemd" "/var/log" "/srv" ];
+    };
+
+    # Add terminfo files
+    enableAllTerminfo = true;
+  };
+
+  # Allows users to allow others on their binds
+  programs.fuse.userAllowOther = true;
+
+  hardware.enableRedistributableFirmware = true;
+
+  # Increase open file limit for sudoers
+  security.pam.loginLimits = [
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "soft";
+      value = "524288";
+    }
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "hard";
+      value = "1048576";
+    }
+  ];
+}
