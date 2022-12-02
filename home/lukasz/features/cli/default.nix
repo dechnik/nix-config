9e596ba1faaf3c0c68447e15d2db7ac5015a0871
Lukasz Dechnik <lukasz@dechnik.net>
2022-12-02 20:37:06 +0100

    basic home-manager

--- home/lukasz/features/cli/default.nix
+++ home/lukasz/features/cli/default.nix
@@ -1,15 +1,15 @@
 { pkgs, ... }: {
   imports = [
-    # ./bash.nix
-    # ./bat.nix
-    # ./fish.nix
-    # ./git.nix
+    ./bash.nix
+    ./bat.nix
+    ./fish.nix
+    ./git.nix
     # ./gpg.nix
     # ./nix-index.nix
     # ./pfetch.nix
     # ./ranger.nix
     # ./screen.nix
-    # ./shellcolor.nix
+    ./shellcolor.nix
     # ./ssh.nix
     # ./starship.nix
     # ./xpo.nix
---
+++ home/lukasz/features/cli/git.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+{
+  programs.git = {
+    enable = true;
+    package = pkgs.gitAndTools.gitFull;
+    aliases = {
+      pushall = "!git remote | xargs -L1 git push --all";
+      graph = "log --decorate --oneline --graph";
+      add-nowhitespace = "!git diff -U0 -w --no-color | !git apply --cached --ignore-whitespace --unidiff-zero -";
+    };
+    userName = "lukasz Dechnik";
+    userEmail = "lukasz@dechnik.net";
+    extraConfig = {
+      feature.manyFiles = true;
+      init.defaultBranch = "master";
+      url."https://github.com/".insteadOf = "git://github.com/";
+    };
+    lfs = { enable = true; };
+    ignores = [ ".direnv" "result" ];
+    signing = {
+      signByDefault = true;
+      key = "D7BCC570927C355B";
+    };
+  };
+}
