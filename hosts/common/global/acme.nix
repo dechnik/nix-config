f3697284115a12a08230451b358488bdc649938f
Lukasz Dechnik <lukasz@dechnik.net>
2022-12-05 12:35:04 +0100

    acme tweaking

--- hosts/common/global/acme.nix
+++ hosts/common/global/acme.nix
@@ -1,9 +1,18 @@
-{ lib, ... }:
+{ lib, config, ... }:
 {
   # Enable acme for usage with nginx vhosts
   security.acme = {
-    defaults.email = "lukasz@dechnik.net";
+    defaults = {
+      email = "lukasz@dechnik.net";
+      dnsProvider = "route53";
+      credentialsFile = config.sops.secrets.acme-credentials.path;
+    };
     acceptTerms = true;
+  };
+
+  sops.secrets.acme-credentials = {
+    sopsFile = ../secrets.yaml;
+    neededForUsers = true;
   };
 
   environment.persistence = {
