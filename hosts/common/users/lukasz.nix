18ca97b283e394d0b70ac165b8d66ba5349bfe67
Lukasz Dechnik <lukasz@dechnik.net>
2022-12-06 12:17:05 +0100

    fix

--- hosts/common/users/lukasz.nix
+++ hosts/common/users/lukasz.nix
@@ -36,7 +36,7 @@ in
     neededForUsers = true;
   };
 
-  home-manager.users.misterio = import ../../../home/lukasz/${config.networking.hostName}.nix;
+  home-manager.users.lukasz = import ../../../home/lukasz/${config.networking.hostName}.nix;
 
   services.geoclue2.enable = true;
   security.pam.services = { swaylock = { }; };
