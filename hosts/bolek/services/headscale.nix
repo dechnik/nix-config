78bbf05278dced63adcbe5be5da883b0acd17aeb
Lukasz Dechnik <lukasz@dechnik.net>
2022-12-07 18:36:42 +0100

    headscale nginx fix

--- hosts/bolek/services/headscale.nix
+++ hosts/bolek/services/headscale.nix
@@ -32,6 +32,14 @@
         locations."/" = {
           proxyPass = "http://localhost:${toString config.services.headscale.port}";
           proxyWebsockets = true;
+          extraConfig = ''
+            proxy_set_header Host $server_name;
+            proxy_buffering off;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $http_x_forwarded_proto;
+            add_header Strict-Transport-Security "max-age=15552000; includeSubDomains" always;
+          '';
         };
       };
     };
