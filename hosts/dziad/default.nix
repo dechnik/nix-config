76487943285f2b41315bd7ef561e1b9324912a40
Lukasz Dechnik <lukasz@dechnik.net>
2022-12-13 15:23:55 +0100

    dziad preparations

---
+++ hosts/dziad/default.nix
@@ -0,0 +1,48 @@
+{ inputs, pkgs, ... }:
+
+{
+  imports =
+    [
+      inputs.hardware.nixosModules.common-cpu-amd
+      inputs.hardware.nixosModules.common-gpu-nvidia
+      inputs.hardware.nixosModules.common-pc-ssd
+
+      ./hardware-configuration.nix
+
+      ../common/global
+      ../common/users/lukasz.nix
+
+      ../common/optional/pipewire.nix
+    ];
+
+  networking = {
+    hostName = "dziad"; # Define your hostname.
+    useDHCP = true;
+    # networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  };
+
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+  };
+
+  programs = {
+    dconf.enable = true;
+  };
+
+  services.dbus.packages = [ pkgs.gcr ];
+
+  xdg.portal = {
+    enable = true;
+  };
+
+  hardware = {
+    opengl = {
+      enable = true;
+      extraPackages = with pkgs; [ amdvlk ];
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+  };
+
+  system.stateVersion = "22.05"; # Did you read the comment?
+}
