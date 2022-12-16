b182106fed9a204282cb9b3a177c0c6b6eb44423
Lukasz Dechnik <lukasz@dechnik.net>
2022-12-16 19:04:54 +0100

    wayland tools fix

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,6 +1,8 @@
 {
   fonts = import ./fonts.nix;
   neovim = import ./neovim.nix;
+  swaylock = import ./swaylock.nix;
+  rgbdaemon = import ./rgbdaemon.nix;
   shellcolor = import ./shellcolor.nix;
   monitors = import ./monitors.nix;
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,7 @@
 { pkgs ? null }: {
   shellcolord = pkgs.callPackage ./shellcolord { };
   lyrics = pkgs.callPackage ./lyrics { };
+  pass-wofi = pkgs.callPackage ./pass-wofi { };
   primary-xwayland = pkgs.callPackage ./primary-xwayland { };
   wl-mirror-pick = pkgs.callPackage ./wl-mirror-pick { };
   plymouth-spinner-monochrome = pkgs.callPackage ./plymouth-spinner-monochrome { };
