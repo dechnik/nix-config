b79bb12c0a73d3a4e29aa2be0fc35543c95ef878
Lukasz Dechnik <lukasz@dechnik.net>
2022-12-16 08:37:00 +0100

    spinner

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,5 @@
 { pkgs ? null }: {
   shellcolord = pkgs.callPackage ./shellcolord { };
   lyrics = pkgs.callPackage ./lyrics { };
+  plymouth-spinner-monochrome = pkgs.callPackage ./plymouth-spinner-monochrome { };
 }
