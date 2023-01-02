40ca7f8fa9bfa36bebc3df5f8153ae83fd476f9e
Lukasz Dechnik <lukasz@dechnik.net>
2023-01-02 08:27:52 +0100

    substituters

--- flake.nix
+++ flake.nix
@@ -85,11 +85,13 @@
       nixConfig = {
         extra-substituters = [
           "https://cache.dechnik.net"
-          # "https://nix-community.cachix.org"
+          "https://hyprland.cachix.org"
+          "https://nix-community.cachix.org"
         ];
         extra-trusted-public-keys = [
           "cache.dechnik.net:VM4JPWTGlfhOxnJsFk1r325lDewW44eyZ32ivqPaFJQ="
-          # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         ];
       };
     };
--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -4,11 +4,13 @@
     settings = {
       substituters = [
         "https://cache.dechnik.net"
-        # "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
         "cache.dechnik.net:VM4JPWTGlfhOxnJsFk1r325lDewW44eyZ32ivqPaFJQ="
-        # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       trusted-users = [ "root" "@wheel" ];
       auto-optimise-store = lib.mkDefault true;
