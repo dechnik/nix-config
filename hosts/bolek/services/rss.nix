ee88323ecc4a0823707487c54a96dab6e47cd78c
Lukasz Dechnik <lukasz@dechnik.net>
2023-01-02 10:48:02 +0100

    bolek rss nginx

--- hosts/bolek/services/rss.nix
+++ hosts/bolek/services/rss.nix
@@ -1,5 +1,12 @@
 { config, lib, pkgs, ... }:
-{
+let
+  rssport = 49999;
+in {
+  security.acme.certs = {
+    "rss.dechnik.net" = {
+      group = "nginx";
+    };
+  };
   virtualisation.oci-containers = {
     backend = "podman";
     containers = {
@@ -11,12 +18,24 @@
       freshrss = {
         autoStart = true;
         image = "lscr.io/linuxserver/freshrss:latest";
-        ports = [ "49999:80" ];
+        ports = [ "${toString rssport}:80" ];
         environment = {
           "TZ" = "Europe/Warsaw";
         };
         volumes = [ "/var/lib/freshrss:/config" ];
       };
+    };
+  };
+  services.nginx.virtualHosts = {
+    "rss.dechnik.net" = {
+      forceSSL = true;
+      useACMEHost = "rss.dechnik.net";
+      locations."/" = {
+        proxyPass = "http://localhost:${toString rssport}";
+      };
+      extraConfig = ''
+        access_log /var/log/nginx/rss.dechnik.net.access.log;
+      '';
     };
   };
   environment.persistence = {
