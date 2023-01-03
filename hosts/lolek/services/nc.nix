c9ac4d9728d90b639ad750987ef7265eb5cdb580
Lukasz Dechnik <lukasz@dechnik.net>
2023-01-03 18:14:44 +0100

    lolek nextcloud tweaking, redis

--- hosts/lolek/services/nc.nix
+++ hosts/lolek/services/nc.nix
@@ -11,14 +11,36 @@ in
       https = true;
       enableBrokenCiphersForSSE = false;
       home = "/media/nextcloud";
+      caching.redis = true;
       config = {
         trustedProxies = ["10.30.10.12"];
         adminuser = "root";
         adminpassFile = config.sops.secrets.nextcloud-password.path;
         dbtype = "pgsql";
+        dbhost = "/run/postgresql";
+        dbname = "nextcloud";
+        dbuser = "nextcloud";
+        defaultPhoneRegion = "PL";
       };
     };
+    postgresql = {
+      enable = true;
+      ensureUsers = [
+        {
+          name = "nextcloud";
+          ensurePermissions = {
+            "DATABASE nextcloud" = "ALL PRIVILEGES";
+          };
+        }
+      ];
+      ensureDatabases = [ "nextcloud" ];
+    };
   };
+  services.redis.servers.nextcloud.enable = true;
+  services.redis.servers.nextcloud.unixSocketPerm = 770;
+
+  users.users.nginx.extraGroups = [ "redis-nextcloud" ];
+  users.users.nextcloud.extraGroups = [ "redis-nextcloud" ];
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 
