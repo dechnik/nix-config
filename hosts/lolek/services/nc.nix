a8553cc53d82edbfa89a6aa576e1f45068c4f3c6
Lukasz Dechnik <lukasz@dechnik.net>
2023-01-03 12:48:51 +0100

    lolek nextcloud

--- hosts/lolek/services/nc.nix
+++ hosts/lolek/services/nc.nix
@@ -1,3 +1,24 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
+let
+  hostName = "nextcloud.dechnik.net";
+in
 {
+  services = {
+    nextcloud = {
+      inherit hostName;
+      package = pkgs.nextcloud25;
+      enable = true;
+      https = true;
+      enableBrokenCiphersForSSE = false;
+      home = "/media/nextcloud";
+      config.adminuser = "root";
+      config.adminpassFile = config.sops.secrets.nextcloud-password.path;
+    };
+  };
+
+  sops.secrets.nextcloud-password = {
+    owner = "nextcloud";
+    group = "nextcloud";
+    sopsFile = ../secrets.yaml;
+  };
 }
