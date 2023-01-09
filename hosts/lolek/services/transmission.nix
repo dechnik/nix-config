19c57df849c6779356189ed598ba049e06847cb2
Lukasz Dechnik <lukasz@dechnik.net>
2023-01-09 19:20:10 +0100

    lolek transmission new media disk

--- hosts/lolek/services/transmission.nix
+++ hosts/lolek/services/transmission.nix
@@ -8,15 +8,11 @@
       downloadDirPermissions = "777";
       settings = {
         rpc-bind-address = "0.0.0.0";
-        download-dir = "/data/new";
-        incomplete-dir = "/data/incomplete";
+        download-dir = "/media/new";
+        incomplete-dir = "/media/incomplete";
         rpc-whitelist = "10.100.0.* 10.10.10.* 10.30.10.* 127.0.0.1 localhost";
         # rpc-host-whitelist = "transmission.i.graysonhead.net";
       };
     };
-  };
-
-  environment.persistence = {
-    "/persist".directories = [ "/data" ];
   };
 }
