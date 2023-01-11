1e1a02421b5d2573e89505ea6d5ca32357db9162
Lukasz Dechnik <lukasz@dechnik.net>
2023-01-11 08:36:02 +0100

    tola desktop basics

--- home/lukasz/tola.nix
+++ home/lukasz/tola.nix
@@ -2,6 +2,9 @@
   imports = [
     ./global
     ./features/cli/tmux.nix
+    ./features/trusted
+    ./features/desktop/hyprland
+    ./features/desktop/terminal
   ];
   colorscheme = inputs.nix-colors.colorSchemes.gruvbox-dark-hard;
 }
