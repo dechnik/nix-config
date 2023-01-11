d5a1ad06f7f8e9cfeb4468195314c3ae08181ed4
Lukasz Dechnik <lukasz@dechnik.net>
2023-01-11 13:26:28 +0100

    tola added gnome

---
+++ hosts/common/optional/gnome.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs.gnomeExtensions; [
+    appindicator
+    # gsconnect
+    # ideapad-mode
+    vitals
+    pkgs.gnome.gnome-tweaks
+  ];
+  services = {
+    xserver = {
+      desktopManager.gnome = {
+        enable = true;
+      };
+      displayManager.gdm = {
+        enable = true;
+        autoSuspend = false;
+      };
+    };
+    geoclue2.enable = true;
+    gnome.games.enable = true;
+  };
+  # Fix broken stuff
+  services.avahi.enable = false;
+  networking.networkmanager.enable = false;
+}
