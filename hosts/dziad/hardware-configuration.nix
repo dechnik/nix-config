ceec2543d848ed8bfe97d74810295676df8c1016
Lukasz Dechnik <lukasz@dechnik.net>
2023-01-26 09:09:14 +0100

    dziad boot device

--- hosts/dziad/hardware-configuration.nix
+++ hosts/dziad/hardware-configuration.nix
@@ -19,12 +19,15 @@
     };
   };
 
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-label/dziad";
-      fsType = "btrfs";
-      options = [ "subvol=boot" ];
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/be1c825f-1e04-4e02-a4d9-bf6676806b76";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot/efi" = {
+    device = "/dev/disk/by-uuid/A2C4-DBDC";
+    fsType = "vfat";
+  };
 
   nixpkgs.hostPlatform.system = "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = true;
