4f0739f74df4481434957b6761aacbebca627de1
lukasz Dechnik <lukasz@dechnik.net>
2023-01-28 21:48:08 +0100

    unifi ports allow

--- hosts/dziad/default.nix
+++ hosts/dziad/default.nix
@@ -22,7 +22,20 @@
     hostName = "dziad"; # Define your hostname.
     useDHCP = true;
     # networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+    firewall = {
+      allowedTCPPorts = [
+        8080 # Port for UAP to inform controller.
+        8880 # Port for HTTP portal redirect, if guest portal is enabled.
+        8843 # Port for HTTPS portal redirect, ditto.
+        6789 # Port for UniFi mobile speed test.
+      ];
+      allowedUDPPorts = [
+        3478 # UDP port used for STUN.
+        10001 # UDP port used for device discovery.
+      ];
+    };
   };
+
 
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_zen;
