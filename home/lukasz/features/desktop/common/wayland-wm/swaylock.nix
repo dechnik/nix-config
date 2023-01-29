938879e90580cd679dcd42c742d6bec2d64012c9
lukasz Dechnik <lukasz@dechnik.net>
2023-01-29 14:07:45 +0100

    blur tweak

--- home/lukasz/features/desktop/common/wayland-wm/swaylock.nix
+++ home/lukasz/features/desktop/common/wayland-wm/swaylock.nix
@@ -6,7 +6,7 @@ in
   home.packages = [ pkgs.swaylock-effects ];
   programs.swaylock = {
     settings = {
-      effect-blur = "20x3";
+      effect-blur = "30x3";
       fade-in = 0.1;
 
       font = config.fontProfiles.regular.family;
