a175f61f891356bbe9fd83a11a44428efced6252
Lukasz Dechnik <lukasz@dechnik.net>
2023-02-01 13:47:26 +0100

    vpn tweaking

--- hosts/common/optional/vpn.nix
+++ hosts/common/optional/vpn.nix
@@ -1,26 +1,27 @@
 { config, lib, pkgs, ... }:
-let
-  connections = [];
-in
 {
   networking.firewall.allowedTCPPorts = [ 1701 ];
   networking.firewall.allowedUDPPorts = [ 1701 500 4500 ];
   services.xl2tpd = {
     enable = true;
-    extraXl2tpOptions = builtins.concatStringsSep "\n" connections;
+    # extraXl2tpOptions = ''
+    #   ${builtins.readFile config.sops.secrets.l2tp-ebi-conn.path}
+    # '';
   };
   sops.secrets = {
-    l2tp-ebi-conn = {
+    l2tp-conn = {
       sopsFile = ../secrets.yaml;
       neededForUsers = true;
     };
     l2tp-ebi-options = {
       sopsFile = ../secrets.yaml;
       neededForUsers = true;
+      path = "/etc/ppp/options.l2tpd.ebi";
     };
     ipsec-secrets = {
       sopsFile = ../secrets.yaml;
       neededForUsers = true;
+      path = "/etc/ipsec.secrets";
     };
   };
 }
