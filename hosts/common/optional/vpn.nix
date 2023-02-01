0165cab3144078eae80d74bf29c6e5b23b953868
Lukasz Dechnik <lukasz@dechnik.net>
2023-02-01 12:36:12 +0100

    vpn branch

--- hosts/common/optional/vpn.nix
+++ hosts/common/optional/vpn.nix
@@ -1,9 +1,13 @@
 { config, lib, pkgs, ... }:
+let
+  connections = [];
+in
 {
   networking.firewall.allowedTCPPorts = [ 1701 ];
   networking.firewall.allowedUDPPorts = [ 1701 500 4500 ];
   services.xl2tpd = {
     enable = true;
+    extraXl2tpOptions = builtins.concatStringsSep "\n" connections;
   };
   sops.secrets = {
     l2tp-ebi-conn = {
