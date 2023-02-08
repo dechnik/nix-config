0aa5d79396c5e1099eee25aff981687dca7e4b2f
Lukasz Dechnik <lukasz@dechnik.net>
2023-02-08 10:09:09 +0100

    headscale tweaking

--- hosts/bolek/services/headscale.nix
+++ hosts/bolek/services/headscale.nix
@@ -9,6 +9,8 @@
   services = {
     headscale = {
       enable = true;
+      port = 8085;
+      address = "127.0.0.1";
       settings = {
         dns_config = {
           override_local_dns = true;
@@ -20,13 +22,13 @@
           ];
         };
         serverUrl = "https://tailscale.dechnik.net";
+        metrics_listen_addr = "127.0.0.1:8095";
         ip_prefixes = [
           "10.100.0.0/16"
         ];
         logtail.enabled = false;
         log.level = "warn";
       };
-      port = 8085;
     };
 
     nginx.virtualHosts = {
@@ -49,7 +51,7 @@
             '';
           };
           "/metrics" = {
-            proxyPass = "http://127.0.0.1:${toString config.services.headscale.port}";
+            proxyPass = "http://${config.services.headscale.settings.metrics_listen_addr}/metrics";
             extraConfig = ''
               allow 10.0.0.0/8;
               deny all;
