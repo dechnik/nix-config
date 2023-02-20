62f7f95f4db60af70d9a5e15e219b124ed6f11c1
lukasz Dechnik <lukasz@dechnik.net>
2023-02-20 10:41:07 +0100

    syncthing added bolek

--- hosts/common/optional/syncthing.nix
+++ hosts/common/optional/syncthing.nix
@@ -9,39 +9,44 @@
     # dataDir = "/media/syncthing";
     # configDir = "/media/syncthing/.config";
     openDefaultPorts = true;
-    # devices = {
-    #   bolek = {
-    #     id = "G2UU2SO-ETG3TAS-D2WXFDU-YZN3DTM-YQCB6I2-DMHKYD5-Q2XDJ3T-Y4LMCQI";
-    #     name = "bolek";
-    #     introducer = true;
-    #   };
-    # };
-    # folders = {
-    #   "/persist/home/lukasz/Downloads" = {
-    #     id = "persistent-home-downloads";
-    #     devices = [ "bolek" ];
-    #   };
-    #   "/persist/home/lukasz/Documents" = {
-    #     id = "persistent-home-documents";
-    #     devices = [ "bolek" ];
-    #   };
-    #   "/persist/home/lukasz/Music" = {
-    #     id = "persistent-home-music";
-    #     devices = [ "bolek" ];
-    #   };
-    #   "/persist/home/lukasz/Pictures" = {
-    #     id = "persistent-home-pictures";
-    #     devices = [ "bolek" ];
-    #   };
-    #   "/persist/home/lukasz/Org" = {
-    #     id = "persistent-home-org";
-    #     devices = [ "bolek" ];
-    #   };
-    #   "/persist/home/lukasz/Roam" = {
-    #     id = "persistent-home-roam";
-    #     devices = [ "bolek" ];
-    #   };
-    # };
+    devices = {
+      dziad = {
+        id = "G2UU2SO-ETG3TAS-D2WXFDU-YZN3DTM-YQCB6I2-DMHKYD5-Q2XDJ3T-Y4LMCQI";
+        name = "dziad";
+        introducer = true;
+      };
+      bolek = {
+        id = "R2GZPQR-N6GOWPS-YLJEDMK-JOFWRMN-GLIHJSE-CTFNXB6-7WYGNCS-GG2KHAO";
+        name = "bolek";
+        introducer = true;
+      };
+    };
+    folders = {
+      "/persist/home/lukasz/Downloads" = {
+        id = "persistent-home-downloads";
+        devices = [ "dziad" "bolek" ];
+      };
+      "/persist/home/lukasz/Documents" = {
+        id = "persistent-home-documents";
+        devices = [ "dziad" "bolek" ];
+      };
+      "/persist/home/lukasz/Music" = {
+        id = "persistent-home-music";
+        devices = [ "dziad" "bolek" ];
+      };
+      "/persist/home/lukasz/Pictures" = {
+        id = "persistent-home-pictures";
+        devices = [ "dziad" "bolek" ];
+      };
+      "/persist/home/lukasz/Org" = {
+        id = "persistent-home-org";
+        devices = [ "dziad" "bolek" ];
+      };
+      "/persist/home/lukasz/Roam" = {
+        id = "persistent-home-roam";
+        devices = [ "dziad" "bolek" ];
+      };
+    };
   };
   sops.secrets = {
     syncthing-cert = {
