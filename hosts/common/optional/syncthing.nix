e17839fb9b540a69254a91c086bc58f15e11a334
lukasz Dechnik <lukasz@dechnik.net>
2023-02-20 11:31:04 +0100

    syncthing new approach

--- hosts/common/optional/syncthing.nix
+++ hosts/common/optional/syncthing.nix
@@ -15,51 +15,32 @@
         name = "dziad";
         introducer = true;
       };
-      bolek = {
-        id = "R2GZPQR-N6GOWPS-YLJEDMK-JOFWRMN-GLIHJSE-CTFNXB6-7WYGNCS-GG2KHAO";
-        name = "bolek";
-        introducer = true;
-      };
     };
     folders = {
       "/persist/home/lukasz/Downloads" = {
         id = "persistent-home-downloads";
-        devices = [ "dziad" "bolek" ];
+        devices = [ "dziad" ];
       };
       "/persist/home/lukasz/Documents" = {
         id = "persistent-home-documents";
-        devices = [ "dziad" "bolek" ];
+        devices = [ "dziad" ];
       };
       "/persist/home/lukasz/Music" = {
         id = "persistent-home-music";
-        devices = [ "dziad" "bolek" ];
+        devices = [ "dziad" ];
       };
       "/persist/home/lukasz/Pictures" = {
         id = "persistent-home-pictures";
-        devices = [ "dziad" "bolek" ];
+        devices = [ "dziad" ];
       };
       "/persist/home/lukasz/Org" = {
         id = "persistent-home-org";
-        devices = [ "dziad" "bolek" ];
+        devices = [ "dziad" ];
       };
       "/persist/home/lukasz/Roam" = {
         id = "persistent-home-roam";
-        devices = [ "dziad" "bolek" ];
+        devices = [ "dziad" ];
       };
-    };
-  };
-  sops.secrets = {
-    syncthing-cert = {
-      sopsFile = ../secrets.yaml;
-      owner = "lukasz";
-      mode = "0400";
-      path = "/run/syncthing-cert.pem";
-    };
-    syncthing-key = {
-      sopsFile = ../secrets.yaml;
-      owner = "lukasz";
-      mode = "0400";
-      path = "/run/syncthing-key.pem";
     };
   };
 }
--- hosts/dziad/default.nix
+++ hosts/dziad/default.nix
@@ -21,6 +21,21 @@
       # ../common/optional/quietboot.nix
     ];
 
+  sops.secrets = {
+    syncthing-cert = {
+      sopsFile = ./secrets.yaml;
+      owner = "lukasz";
+      mode = "0400";
+      path = "/run/syncthing-cert.pem";
+    };
+    syncthing-key = {
+      sopsFile = ./secrets.yaml;
+      owner = "lukasz";
+      mode = "0400";
+      path = "/run/syncthing-key.pem";
+    };
+  };
+
   networking = {
     hostName = "dziad"; # Define your hostname.
     useDHCP = true;
