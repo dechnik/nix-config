4a37cd9251ce26eb3aada1bc75ead93e281114ad
lukasz Dechnik <lukasz@dechnik.net>
2023-02-27 15:54:20 +0100

    chromium command line args

--- home/lukasz/features/desktop/common/chromium.nix
+++ home/lukasz/features/desktop/common/chromium.nix
@@ -9,5 +9,13 @@
       {id = "dbepggeogbaibhgnhhndojpepiihcmeb";} #vimium
       {id = "naepdomgkenhinolocfifgehidddafch";} #browserpass
     ];
+    commandLineArgs = [
+      "--disable-features=UseChromeOSDirectVideoDecoder"
+      "--enable-features=VaapiVideoEncoder,VaapiVideoDecoder"
+      "--enable-zero-copy"
+      "--disable-gpu-driver-bug-workarounds"
+      "--use-gl=egl"
+      "--ignore-gpu-blocklist"
+    ];
   };
 }
