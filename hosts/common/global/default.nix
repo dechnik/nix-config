1a71ed732a0c5e18696d00dd4deb34ba28fc9a0b
lukasz Dechnik <lukasz@dechnik.net>
2023-03-03 10:57:33 +0100

    common fail2ban enabled

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -4,6 +4,7 @@
     inputs.impermanence.nixosModules.impermanence
     inputs.home-manager.nixosModules.home-manager
     ./acme.nix
+    ./fail2ban.nix
     ./firewall.nix
     ./fish.nix
     ./zsh.nix
---
+++ hosts/common/global/fail2ban.nix
@@ -0,0 +1,12 @@
+{config, ...}: {
+  services.fail2ban = {
+    enable = config.networking.firewall.enable;
+    maxretry = 5;
+    ignoreIP = [
+      "127.0.0.0/8"
+      "10.0.0.0/8"
+      "172.16.0.0/12"
+      "192.168.0.0/16"
+    ];
+  };
+}
