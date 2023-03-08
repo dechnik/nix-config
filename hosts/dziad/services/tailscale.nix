e6c89d91b502a67be7fe865139c4d245e80a27c4
lukasz Dechnik <lukasz@dechnik.net>
2023-03-08 19:34:21 +0100

    dziad tailscale

--- hosts/dziad/services/tailscale.nix
+++ hosts/dziad/services/tailscale.nix
@@ -5,7 +5,7 @@
   ...
 }:(import ../../common/functions/tailscale.nix {inherit config pkgs lib;}).tailscale
   {
-    preAuthKey = "7a82166697b0c2298b7cd15626f789f40263d6d03bb33b87"; # onetime key
+    preAuthKey = "7502c12583d639a03c53cd4d439cd2bbaf5f69c9ff46fa05"; # onetime key
     loginServer = "https://tailscale.dechnik.net";
     reauth = false;
     exitNode = false;
