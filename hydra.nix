bc8507c147c03e1482b80a14911d7136b58c8537
lukasz Dechnik <lukasz@dechnik.net>
2023-03-10 15:46:30 +0100

    hydraJobs fix for dot in job name

--- hydra.nix
+++ hydra.nix
@@ -1,23 +1,16 @@
 { inputs, outputs }:
 let
-  inherit (inputs.nixpkgs.lib) filterAttrs mapAttrs elem;
+  inherit (inputs.nixpkgs.lib) replaceStrings filterAttrs nameValuePair mapAttrs elem;
 
   notBroken = pkg: !(pkg.meta.broken or false);
   hasPlatform = sys: pkg: elem sys pkg.meta.platforms;
   filterValidPkgs = sys: pkgs: filterAttrs (_: pkg: hasPlatform sys pkg && notBroken pkg) pkgs;
-  getCfg = name: cfg: cfg.config.system.build.toplevel;
+  mangleName = replaceStrings [ "." ] [ "_" ];
+  # getCfg = name: cfg: cfg.config.system.build.toplevel;
+  getCfg = name: config: nameValuePair (mangleName name) config.config.system.build.toplevel;
 in
 {
   pkgs = mapAttrs filterValidPkgs outputs.packages;
-  hosts = mapAttrs getCfg outputs.nixosConfigurations;
 }
-#{ self, nixpkgs, flake-utils, ... }:
-#(nixpkgs.lib.mapAttrs'
-#  (name: config: nixpkgs.lib.nameValuePair name config.config.system.build.toplevel)
-#  self.nixosConfigurations
-#)
-#    hydraJobs = lib.genAttrs hydraSystems (system: let
-#      # Hydra doesn't allow job names containing periods.
-#      mangleName = lib.replaceStrings [ "." ] [ "_" ];
-#      mangleAttrName = name: lib.nameValuePair (mangleName name);
-#    in lib.mapAttrs' mangleAttrName self.checks.${system});
+  //
+  inputs.nixpkgs.lib.mapAttrs' getCfg outputs.nixosConfigurations
