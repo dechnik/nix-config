43db6be8da5f48486367c1e74490731d147bd6e9
lukasz Dechnik <lukasz@dechnik.net>
2023-03-14 19:35:51 +0100

    flakes update

--- common/global/firewall.nix
+++ common/global/firewall.nix
@@ -6,7 +6,6 @@
     checkReversePath = lib.mkDefault "loose";
     logRefusedConnections = lib.mkDefault false;
 
-    autoLoadConntrackHelpers = true;
     connectionTrackingModules = [
       "ftp"
       "tftp"
