b24564912b7d6dc6348a9195bae6f5cb13ff3474
lukasz Dechnik <lukasz@dechnik.net>
2023-03-15 21:23:01 +0100

    lolek net config, default trough bolek nat

--- hosts/lolek.pve/default.nix
+++ hosts/lolek.pve/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 
 {
   imports =
@@ -9,14 +9,40 @@
       ../../common/optional/qemu-vm.nix
       ../../common/optional/postfix.nix
       ../../common/global
+      ../../common/global/network.nix
       ../../common/users/lukasz
     ];
 
+  my.lan = "ens19";
+
   networking = {
-    hostName = "lolek"; # Define your hostname.
+    hostName = "lolek";
     domain = "pve.dechnik.net";
-    useDHCP = true;
-    # networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+    nameservers = [
+      "10.60.0.1"
+    ];
+    defaultGateway = "10.60.0.1";
+    defaultGateway6 = "";
+    dhcpcd.enable = false;
+    usePredictableInterfaceNames = lib.mkForce true;
+    interfaces."ens18" = {
+      useDHCP = true;
+    };
+    interfaces."${config.my.lan}" = {
+      useDHCP = false;
+      ipv4.addresses = [
+        {
+          address = "10.60.0.2";
+          prefixLength = 24;
+        }
+      ];
+      ipv4.routes = [
+        {
+          address = "10.60.0.1";
+          prefixLength = 32;
+        }
+      ];
+    };
   };
 
   system.stateVersion = "22.05"; # Did you read the comment?
