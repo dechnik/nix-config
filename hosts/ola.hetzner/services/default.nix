c9b664be2de9f68b5579402847523615551da2b1
lukasz Dechnik <lukasz@dechnik.net>
2023-03-21 13:08:07 +0100

    ola restic setup

--- hosts/ola.hetzner/services/default.nix
+++ hosts/ola.hetzner/services/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./mail.nix
+    ./restic.nix
     ./tailscale.nix
     ./wireguard.nix
   ];
---
+++ hosts/ola.hetzner/services/restic.nix
@@ -0,0 +1,21 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  restic = import ../../../common/functions/restic.nix {inherit config lib pkgs;};
+
+  paths = [
+    "/srv/mail"
+  ];
+
+  cfg = site: {
+    secret = "restic-ola-pve-token";
+    inherit site;
+    inherit paths;
+  };
+in
+  lib.mkMerge [
+    (restic.backupJob (cfg "pve"))
+  ]
