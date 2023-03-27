730e01d967a66d86173a6646c131852df8f400dc
lukasz Dechnik <lukasz@dechnik.net>
2023-03-27 14:09:09 +0200

    colmena tweaking

--- colmena.nix
+++ colmena.nix
@@ -27,6 +27,8 @@
         then true
         else false;
       allowLocalDeployment = true;
+      targetUser = "lukasz";
+      # targetHost = value.config.networking.fqdn;
     };
     nixpkgs.system = value.config.nixpkgs.hostPlatform.system;
     imports = value._module.args.modules;
