84369570325a7fdda56b1eb40f8177eb9e9b54df
lukasz Dechnik <lukasz@dechnik.net>
2023-04-04 19:56:54 +0200

    website nix

--- hosts/bolek.pve/services/website.nix
+++ hosts/bolek.pve/services/website.nix
@@ -32,6 +32,24 @@ in
               add_header Cache-Control "max-age=${days 30}";
             '';
           };
+          "=/nix" = {
+            # Script to download static nix
+            # Sadly only works on linux
+            alias = pkgs.writeText "nix" ''
+              #/bin/sh
+
+              arch="$(uname -m)"
+              job="https://hydra.nixos.org/job/nix/master/buildStatic.$arch-linux/latest/download-by-type/file/binary-dist"
+              echo "Downloading from: $job"
+
+              mkdir -p "$HOME/.local/bin"
+              curl -L "$job" -o "$HOME/.local/bin/nix"
+              chmod +x "$HOME/.local/bin/nix"
+
+              mkdir -p "$HOME/.config/nix"
+              echo "experimental-features = nix-command flakes" >> "$HOME/.config/nix/nix.conf"
+            '';
+          };
         };
       };
     };
