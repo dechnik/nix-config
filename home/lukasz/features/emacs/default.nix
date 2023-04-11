f2c1dfe5f59ed493bc0b88df2875a9a6707c1fc0
lukasz Dechnik <lukasz@dechnik.net>
2023-04-11 11:06:45 +0200

    emacs: install doom on activation if not installed

--- home/lukasz/features/emacs/default.nix
+++ home/lukasz/features/emacs/default.nix
@@ -156,12 +156,12 @@ in
     ];
   };
 
-  # home.activation = {
-  #   installDoomEmacs = ''
-  #     export DOOMDIR="${config.home.sessionVariables.DOOMDIR}"
-  #     if [ ! -d "${h}/.emacs.d" ]; then
-  #       git clone https://github.com/doomemacs/doomemacs.git ${h}/.emacs.d
-  #     fi
-  #   '';
-  # };
+  home.activation = {
+    installDoomEmacs = ''
+      export DOOMDIR="${config.home.sessionVariables.DOOMDIR}"
+      if [ ! -d "${h}/.emacs.d" ]; then
+        git clone --depth=1 https://github.com/doomemacs/doomemacs.git ${h}/.emacs.d
+      fi
+    '';
+  };
 }
