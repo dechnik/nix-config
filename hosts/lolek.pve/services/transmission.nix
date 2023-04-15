823c974ef3490d0d29414953eed42a72aa9e3199
lukasz Dechnik <lukasz@dechnik.net>
2023-04-15 18:40:51 +0200

    lolek.pve transmission tweaking

--- hosts/lolek.pve/services/transmission.nix
+++ hosts/lolek.pve/services/transmission.nix
@@ -7,11 +7,12 @@
       openRPCPort = true;
       downloadDirPermissions = "777";
       settings = {
+        rpc-port = 9091;
         umaks = 0;
         rpc-bind-address = "0.0.0.0";
         download-dir = "/media/new";
         incomplete-dir = "/media/incomplete";
-        rpc-whitelist = "10.*.*.* 127.0.0.1 localhost";
+        rpc-whitelist = "100.*.*.* 10.*.*.* 127.0.0.1 localhost";
         # rpc-host-whitelist = "transmission.i.graysonhead.net";
       };
     };
