15731751f49ebcb6b25934d68e929f8eaba7129b
lukasz Dechnik <lukasz@dechnik.net>
2023-04-17 15:14:03 +0200

    lldap pkg testing

--- pkgs/lldap/default.nix
+++ pkgs/lldap/default.nix
@@ -13,23 +13,39 @@ rustPlatform.buildRustPackage rec {
   version = "0.5.0";
   revision = "d55d4487ed17211cdde96dbc70cfbaaa4825bc26";
 
-  srcs = [
-    (fetchFromGitHub {
+  src = fetchFromGitHub {
       name = pname;
       owner = "lldap";
       repo = pname;
       # rev = "v${version}";
       rev = "${revision}";
       sha256 = "sha256-j5zu5lqGpaxoiTRJVIQrhyHUpIkngIcy/9CsrMmu8Ww=";
-    })
-    # (fetchTarball {
-    #   name = "${pname}-app";
-    #   url = "https://github.com/nitnelave/lldap/releases/download/v${version}/lldap-x86_64-v${version}.tar.gz";
-    #   sha256 = "1cczmr7pjldhchlkmbkzggr28calakqv8rmsqj7ipv1m1hp7mbig";
-    # })
-  ];
+    };
+  # srcs = [
+  #   (fetchFromGitHub {
+  #     name = pname;
+  #     owner = "lldap";
+  #     repo = pname;
+  #     # rev = "v${version}";
+  #     rev = "${revision}";
+  #     sha256 = "sha256-j5zu5lqGpaxoiTRJVIQrhyHUpIkngIcy/9CsrMmu8Ww=";
+  #   })
+  #   # (fetchTarball {
+  #   #   name = "${pname}-app";
+  #   #   url = "https://github.com/nitnelave/lldap/releases/download/v${version}/lldap-x86_64-v${version}.tar.gz";
+  #   #   sha256 = "1cczmr7pjldhchlkmbkzggr28calakqv8rmsqj7ipv1m1hp7mbig";
+  #   # })
+  # ];
 
   sourceRoot = pname;
+  cargoLock = {
+    lockFile = "${src.out}/Cargo.lock";
+    outputHashes = {
+      "lber-0.4.1" = "sha256-2rGTpg8puIAXggX9rEbXPdirfetNOHWfFc80xqzPMT4=";
+      "opaque-ke-0.6.1" = "sha256-99gaDv7eIcYChmvOKQ4yXuaGVzo2Q6BcgSQOzsLF+fM=";
+      "yew_form-0.1.8" = "sha256-1n9C7NiFfTjbmc9B5bDEnz7ZpYJo9ZT8/dioRXJ65hc=";
+    };
+  };
 
   cargoSha256 = "sha256-pO0kEVzgfOGn4PBzTrUyVfcelS+W6RfkYURTUXpms2k=";
   # cargoSha256 = lib.fakeSha256;
