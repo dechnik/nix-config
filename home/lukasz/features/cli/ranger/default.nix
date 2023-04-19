8da64cafc6337800f4344c8d6400f71d9f3db38a
lukasz Dechnik <lukasz@dechnik.net>
2023-04-19 10:46:21 +0200

    home ranger tweaking

--- home/lukasz/features/cli/ranger/default.nix
+++ home/lukasz/features/cli/ranger/default.nix
@@ -2,4 +2,8 @@
   home.packages = with pkgs; [ ranger ];
   home.file.".config/ranger/rc.conf".source = ./rc.conf;
   home.file.".config/ranger/plugins".source = ./plugins;
+  home.file.".config/ranger/commands.py".source = ./commands.py;
+  home.file.".config/ranger/commands_full.py".source = ./commands_full.py;
+  home.file.".config/ranger/rifle.conf".source = ./rifle.conf;
+  home.file.".config/ranger/scope.sh".source = ./scope.sh;
 }
