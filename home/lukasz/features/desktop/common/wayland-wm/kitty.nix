dc1c36dd9e962924f9ea3e1bfe707692d9f384ed
lukasz Dechnik <lukasz@dechnik.net>
2023-04-26 20:25:37 +0200

    kitty tweaking

--- home/lukasz/features/desktop/common/wayland-wm/kitty.nix
+++ home/lukasz/features/desktop/common/wayland-wm/kitty.nix
@@ -22,7 +22,7 @@ in
       size = 10;
     };
     settings = {
-      shell_integration = "disabled"; # I prefer to do it manually
+      shell_integration = "no-rc"; # I prefer to do it manually
       scrollback_lines = 4000;
       scrollback_pager_history_size = 2048;
       enable_audio_bell = "no";
