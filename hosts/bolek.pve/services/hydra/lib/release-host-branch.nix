049af578cf6df7e12acb1c34a3a7fac58e3b0ffe
lukasz Dechnik <lukasz@dechnik.net>
2023-04-27 12:00:42 +0200

    hydra release host script tweaking

--- hosts/bolek.pve/services/hydra/lib/release-host-branch.nix
+++ hosts/bolek.pve/services/hydra/lib/release-host-branch.nix
@@ -16,9 +16,10 @@
     echo "System is $host at $commit"
 
     # Start ssh-agent if nescessary
-    ssh-add || eval "$(ssh-agent)"
-    ssh-add ${sshKeyFile}
-    export GIT_SSH_COMMAND="ssh -A" # Forward agent
+    # ssh-add || eval "$(ssh-agent)"
+    # ssh-add ${sshKeyFile}
+    # export GIT_SSH_COMMAND="ssh -A" # Forward agent
+    export GIT_SSH_COMMAND="ssh -i ${sshKeyFile}"
 
     repo="/tmp/hydra/nix-config"
 
@@ -27,7 +28,7 @@
       git -C "$repo" fetch origin
     else
       mkdir -p "$repo"
-      git clone --bare gitea@git.dechnik.net:lukasz/nix-config.git "$repo"
+      git clone --bare gitea@bolek.pve.dechnik.net:lukasz/nix-config.git "$repo"
     fi
 
     git -C "$repo" branch -f "release-$host" "$commit"
