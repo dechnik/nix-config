58fac415de33342eaadd5e1f12a8015fde6d1fff
lukasz Dechnik <lukasz@dechnik.net>
2023-04-27 17:51:01 +0200

    dziad disable gamemode

--- hosts/dziad/default.nix
+++ hosts/dziad/default.nix
@@ -21,7 +21,7 @@
       ../../common/optional/bluetooth.nix
       ../../common/optional/syncthing.nix
       # ../common/optional/postfix.nix
-      ../../common/optional/gamemode.nix
+      # ../../common/optional/gamemode.nix
       # ../common/optional/zfs.nix
       # ../common/optional/quietboot.nix
     ];
