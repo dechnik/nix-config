59b6b03c4c47b50b0354427c4202c175d676a5e0
lukasz Dechnik <lukasz@dechnik.net>
2023-04-28 12:31:30 +0200

    emacs added zotero

--- home/lukasz/features/emacs/default.nix
+++ home/lukasz/features/emacs/default.nix
@@ -97,6 +97,8 @@ in
     # pinentry_emacs # in-emacs gnupg prompts
     zstd # for undo-fu-session/undo-tree compression
 
+    zotero
+
     ## Module dependencies
     # :checkers spell
     (aspellWithDicts (ds: with ds; [ en en-computers en-science ]))
