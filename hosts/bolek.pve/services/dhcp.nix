55986756dd0ba046e1b0ab697bd204e8f67cd82a
lukasz Dechnik <lukasz@dechnik.net>
2023-05-05 10:48:07 +0200

    bolek.pve dhcp switch from dhcp4 to kea

--- hosts/bolek.pve/services/dhcp.nix
+++ hosts/bolek.pve/services/dhcp.nix
@@ -1,46 +1,46 @@
 { config, ... }: {
-  services.dhcpd4 = {
-    enable = true;
-    interfaces = [ config.my.lan ];
-    extraConfig = ''
-      option subnet-mask 255.255.255.0;
-      subnet 10.60.0.0 netmask 255.255.255.0 {
-        option broadcast-address 10.60.0.255;
-        option domain-name-servers 10.60.0.1;
-        option routers 10.60.0.1;
-        interface ${config.my.lan};
-        range 10.60.0.171 10.60.0.250;
-      }
-    '';
-    machines = [
-    ];
-  };
-  # services.kea.dhcp4 = {
+  # services.dhcpd4 = {
   #   enable = true;
-  #   settings = {
-  #     interfaces-config = {
-  #       interfaces = [
-  #         config.my.lan
-  #       ];
-  #     };
-  #     lease-database = {
-  #       name = "/var/lib/kea/dhcp4.leases";
-  #       persist = true;
-  #       type = "memfile";
-  #     };
-  #     rebind-timer = 2000;
-  #     renew-timer = 1000;
-  #     subnet4 = [
-  #       {
-  #         pools = [
-  #           {
-  #             pool = "10.60.0.171 - 10.60.0.250";
-  #           }
-  #         ];
-  #         subnet = "10.60.0.0/24";
-  #       }
-  #     ];
-  #     valid-lifetime = 4000;
-  #   };
+  #   interfaces = [ config.my.lan ];
+  #   extraConfig = ''
+  #     option subnet-mask 255.255.255.0;
+  #     subnet 10.60.0.0 netmask 255.255.255.0 {
+  #       option broadcast-address 10.60.0.255;
+  #       option domain-name-servers 10.60.0.1;
+  #       option routers 10.60.0.1;
+  #       interface ${config.my.lan};
+  #       range 10.60.0.171 10.60.0.250;
+  #     }
+  #   '';
+  #   machines = [
+  #   ];
   # };
+  services.kea.dhcp4 = {
+    enable = true;
+    settings = {
+      interfaces-config = {
+        interfaces = [
+          config.my.lan
+        ];
+      };
+      lease-database = {
+        name = "/var/lib/kea/dhcp4.leases";
+        persist = true;
+        type = "memfile";
+      };
+      rebind-timer = 2000;
+      renew-timer = 1000;
+      subnet4 = [
+        {
+          pools = [
+            {
+              pool = "10.60.0.171 - 10.60.0.250";
+            }
+          ];
+          subnet = "10.60.0.0/24";
+        }
+      ];
+      valid-lifetime = 4000;
+    };
+  };
 }
