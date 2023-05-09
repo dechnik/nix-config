453c5846218cb784ee1079209d31c1d4f5acfd5e
lukasz Dechnik <lukasz@dechnik.net>
2023-05-09 21:23:55 +0200

    tolek.oracle traefik dashboard setup

--- hosts/tolek.oracle/services/traefik.nix
+++ hosts/tolek.oracle/services/traefik.nix
@@ -2,6 +2,27 @@
 let
   traefik = import ../../../common/functions/traefik.nix { inherit config lib pkgs; };
 in
-{ } // (traefik.traefik {
+{
+  services.traefik.dynamicConfigOptions.http = {
+    serversTransports = {
+      insecure-skip-verify.insecureSkipVerify = true;
+    };
+
+    routers.dashboard = {
+      rule = "Host(`traefik.oracle.dechnik.net`) ";
+      service = "api@internal";
+      entryPoints = [ "web" ];
+      middlewares = [ "tailscale-ips" "auth" ];
+    };
+
+    middlewares.tailscale-ips = {
+      ipWhiteList.sourceRange = [ "100.64.0.0/10" "fd7a:115c:a1e0::/48" ];
+    };
+  };
+
+  services.authelia.instances.main.settings.access_control.rules = [
+    { domain = "traefik.oracle.dechnik.net"; subject = [ "group:admin" ]; policy = "one_factor"; }
+  ];
+} // (traefik.traefik {
   site = "oracle";
 })
