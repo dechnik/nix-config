770bc706b4b16a7c027da72453eba8cfc2e8f75d
lukasz Dechnik <lukasz@dechnik.net>
2023-05-10 12:22:41 +0200

    tolek.oracle lldap move to traefik

--- hosts/tolek.oracle/services/lldap.nix
+++ hosts/tolek.oracle/services/lldap.nix
@@ -1,47 +1,54 @@
-{ lib, inputs, config, ... }:
-# let
-#   nginx = import ../../../common/functions/nginx.nix { inherit config lib; };
-# in
+{ inputs, config, ... }:
 {
   imports = [
     inputs.lldap.nixosModules.default
   ];
   disabledModules = [ "services/databases/lldap.nix" ];
-}
-  // lib.mkMerge [
-  {
-    sops.secrets = {
-      lldap-jwt-secret = {
-        owner = "lldap";
-        group = "lldap";
-        sopsFile = ../secrets.yaml;
-      };
-      lldap-admin-pass = {
-        owner = "lldap";
-        group = "lldap";
-        sopsFile = ../secrets.yaml;
-      };
+
+  sops.secrets = {
+    lldap-jwt-secret = {
+      owner = "lldap";
+      group = "lldap";
+      sopsFile = ../secrets.yaml;
     };
+    lldap-admin-pass = {
+      owner = "lldap";
+      group = "lldap";
+      sopsFile = ../secrets.yaml;
+    };
+  };
+  services.lldap = {
+    enable = true;
+    jwtSecretFile = config.sops.secrets.lldap-jwt-secret.path;
+    userPassFile = config.sops.secrets.lldap-admin-pass.path;
+    dataDir = "/srv/lldap";
+    ldapHost = "0.0.0.0";
+    ldapPort = 3890;
+    httpHost = "127.0.0.1";
+    httpPort = 17170;
+    openFirewall = true;
+    httpUrl = "https://ldap.dechnik.net";
+    ldapBaseDn = "dc=dechnik,dc=net";
+    ldapUserDn = "admin";
+    ldapUserEmail = "admin@dechnik.net";
+  };
+
+  services.traefik.dynamicConfigOptions.http = {
     services.lldap = {
-      enable = true;
-      jwtSecretFile = config.sops.secrets.lldap-jwt-secret.path;
-      userPassFile = config.sops.secrets.lldap-admin-pass.path;
-      dataDir = "/srv/lldap";
-      ldapHost = "0.0.0.0";
-      ldapPort = 3890;
-      httpHost = "127.0.0.1";
-      httpPort = 17170;
-      openFirewall = true;
-      httpUrl = "https://ldap.dechnik.net";
-      ldapBaseDn = "dc=dechnik,dc=net";
-      ldapUserDn = "admin";
-      ldapUserEmail = "admin@dechnik.net";
+      loadBalancer.servers = [{ url = "http://127.0.0.1:17170"; }];
     };
-  }
+
+    routers.lldap = {
+      rule = "Host(`ldap.dechnik.net`)";
+      service = "lldap";
+      entryPoints = [ "web" ];
+      middlewares = [ "tailscale-ips" ];
+    };
+  };
+}
 
   # (nginx.internalVhost {
   #   domain = "ldap.dechnik.net";
   #   tailscaleAuth = false;
   #   proxyPass = "http://127.0.0.1:${toString config.services.lldap.httpPort}";
   # })
-]
