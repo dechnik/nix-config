22792b0b23382a26b309210513ca930fd477c1b3
lukasz Dechnik <lukasz@dechnik.net>
2023-05-10 10:09:51 +0200

    tolek.oracle traefik dashboard tweaking

--- hosts/tolek.oracle/services/traefik.nix
+++ hosts/tolek.oracle/services/traefik.nix
@@ -2,27 +2,29 @@
 let
   traefik = import ../../../common/functions/traefik.nix { inherit config lib pkgs; };
 in
-{
-  services.traefik.dynamicConfigOptions.http = {
-    serversTransports = {
-      insecure-skip-verify.insecureSkipVerify = true;
+lib.mkMerge [
+  {
+    services.traefik.dynamicConfigOptions.http = {
+      serversTransports = {
+        insecure-skip-verify.insecureSkipVerify = true;
+      };
+
+      routers.dashboard = {
+        rule = "Host(`traefik.oracle.dechnik.net`) ";
+        service = "api@internal";
+        entryPoints = [ "web" ];
+        middlewares = [ "tailscale-ips" "auth" ];
+      };
+
+      middlewares.tailscale-ips = {
+        ipWhiteList.sourceRange = [ "100.64.0.0/10" "fd7a:115c:a1e0::/48" ];
+      };
     };
 
-    routers.dashboard = {
-      rule = "Host(`traefik.oracle.dechnik.net`) ";
-      service = "api@internal";
-      entryPoints = [ "web" ];
-      middlewares = [ "tailscale-ips" "auth" ];
-    };
+    services.authelia.instances.main.settings.access_control.rules = [
+      { domain = "traefik.oracle.dechnik.net"; subject = [ "group:admin" ]; policy = "one_factor"; }
+    ];
+  }
 
-    middlewares.tailscale-ips = {
-      ipWhiteList.sourceRange = [ "100.64.0.0/10" "fd7a:115c:a1e0::/48" ];
-    };
-  };
-
-  services.authelia.instances.main.settings.access_control.rules = [
-    { domain = "traefik.oracle.dechnik.net"; subject = [ "group:admin" ]; policy = "one_factor"; }
-  ];
-} // (traefik.traefik {
-  site = "oracle";
-})
+  (traefik.traefik {site = "oracle";})
+]
