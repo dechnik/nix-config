5ee1d380b34855fade38d09536721ed58a0bb056
lukasz Dechnik <lukasz@dechnik.net>
2023-05-19 10:40:34 +0200

    auto-upgrade tweaking

--- common/optional/auto-upgrade.nix
+++ common/optional/auto-upgrade.nix
@@ -2,10 +2,13 @@
 let
   toplevel = builtins.replaceStrings [".dechnik.net"] [""] config.networking.fqdn;
   hostname = builtins.replaceStrings ["."] ["_"] toplevel;
+  # Only enable auto upgrade if current config came from a clean tree
+  # This avoids accidental auto-upgrades when working locally.
+  isClean = inputs.self ? rev;
 in
 {
   system.autoUpgrade = {
-    enable = true;
+    enable = isClean;
     dates = "daily";
     flags = [
       "--refresh"
