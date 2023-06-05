806b5f2e727252fb4a13c5c64485566e424d9d7e
lukasz Dechnik <lukasz@dechnik.net>
2023-06-05 12:18:39 +0200

    password-store-sync

--- home/lukasz/features/trusted/pass.nix
+++ home/lukasz/features/trusted/pass.nix
@@ -18,4 +18,41 @@
   home.persistence = {
     "/persist/home/lukasz".directories = [ ".local/share/password-store" ];
   };
+
+  # systemd.user.services.password-store-sync = {
+  #   Unit = {
+  #     Description = "Password store sync";
+  #   };
+
+  #   Service = {
+  #     CPUSchedulingPolicy = "idle";
+  #     IOSchedulingClass = "idle";
+  #     Environment =
+  #       let
+  #         makeEnvironmentPairs =
+  #           lib.mapAttrsToList (key: value: "${key}=${builtins.toJSON value}");
+  #       in
+  #         makeEnvironmentPairs config.programs.password-store.settings;
+  #     ExecStart = toString (pkgs.writeShellScript "password-store-sync" ''
+  #       ${config.programs.password-store.package}/bin/pass git pull --rebase && \
+  #       ${config.programs.password-store.package}/bin/pass git push
+  #     '');
+  #   };
+  # };
+
+  # systemd.user.timers.password-store-sync = {
+  #   Unit = {
+  #     Description = "Password store periodic sync";
+  #   };
+
+  #   Timer = {
+  #     Unit = "password-store-sync.service";
+  #     OnCalendar = "*:0/10";
+  #     Persistent = true;
+  #   };
+
+  #   Install = {
+  #     WantedBy = [ "timers.target" ];
+  #   };
+  # };
 }
