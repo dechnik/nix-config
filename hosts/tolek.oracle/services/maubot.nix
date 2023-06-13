2ec3f3a6a8eb124dda929933155291533d1decd6
lukasz Dechnik <lukasz@dechnik.net>
2023-06-13 10:47:09 +0200

    tolek.oracle maubot tweaking

--- hosts/tolek.oracle/services/maubot.nix
+++ hosts/tolek.oracle/services/maubot.nix
@@ -20,23 +20,23 @@
 
   services.maubot = {
     enable = true;
-    serverHostname = "127.0.0.1";
+    serverHostname = "10.61.0.1";
     serverPort = 29316;
     publicUrl = "https://matrix.dechnik.net";
     dataDir = "/var/lib/maubot";
     secretYAML = config.sops.secrets.maubot-config.path;
   };
-  services.traefik.dynamicConfigOptions.http = {
-    services.maubot = {
-      loadBalancer.servers = [{ url = "http://127.0.0.1:29316"; }];
-    };
+  # services.traefik.dynamicConfigOptions.http = {
+  #   services.maubot = {
+  #     loadBalancer.servers = [{ url = "http://127.0.0.1:29316"; }];
+  #   };
 
-    routers.maubot = {
-      rule = "(Host(`matrix.dechnik.net`) && (PathPrefix(`/_matrix/maubot`)))";
-      service = "maubot";
-      priority = 999;
-      entryPoints = [ "web" ];
-      middlewares = [ "dechnik-ips" ];
-    };
-  };
+  #   routers.maubot = {
+  #     rule = "(Host(`matrix.dechnik.net`) && (PathPrefix(`/_matrix/maubot`)))";
+  #     service = "maubot";
+  #     priority = 999;
+  #     entryPoints = [ "web" ];
+  #     middlewares = [ "dechnik-ips" ];
+  #   };
+  # };
 }
