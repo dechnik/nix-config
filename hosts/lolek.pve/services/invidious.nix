072722b092c37853aa9d54bda8a817dbcaa45f3e
lukasz Dechnik <lukasz@dechnik.net>
2023-07-20 11:29:06 +0200

    lolek.pve invidious switch to traefik setup

--- hosts/lolek.pve/services/invidious.nix
+++ hosts/lolek.pve/services/invidious.nix
@@ -9,27 +9,27 @@ in {
     neededForUsers = true;
     mode = "0444";
   };
-  security.acme.certs."${domain}" = {
-    domain = domain;
-    group = "nginx";
-  };
-  services.nginx.virtualHosts = {
-    "${domain}" = {
-      forceSSL = true;
-      useACMEHost = "${domain}";
-      locations."/" = {
-        proxyPass = "http://localhost:${toString port}";
-        proxyWebsockets = true;
-      };
-      extraConfig = ''
-        proxy_set_header X-Forwarded-For $remote_addr;
-        proxy_set_header Host $host;    # so Invidious knows domain
-        proxy_http_version 1.1;     # to keep alive
-        proxy_set_header Connection ""; # to keep alive
-        access_log /var/log/nginx/${domain}.access.log;
-      '';
-    };
-  };
+  # security.acme.certs."${domain}" = {
+  #   domain = domain;
+  #   group = "nginx";
+  # };
+  # services.nginx.virtualHosts = {
+  #   "${domain}" = {
+  #     forceSSL = true;
+  #     useACMEHost = "${domain}";
+  #     locations."/" = {
+  #       proxyPass = "http://localhost:${toString port}";
+  #       proxyWebsockets = true;
+  #     };
+  #     extraConfig = ''
+  #       proxy_set_header X-Forwarded-For $remote_addr;
+  #       proxy_set_header Host $host;    # so Invidious knows domain
+  #       proxy_http_version 1.1;     # to keep alive
+  #       proxy_set_header Connection ""; # to keep alive
+  #       access_log /var/log/nginx/${domain}.access.log;
+  #     '';
+  #   };
+  # };
 
   services.invidious = {
     enable = true;
