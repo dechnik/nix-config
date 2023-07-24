c4a60f9c6aabb1f5f8928c2acfabb3baa40def91
lukasz Dechnik <lukasz@dechnik.net>
2023-07-24 08:32:58 +0200

    optional zram

--- hosts/dziad/default.nix
+++ hosts/dziad/default.nix
@@ -21,6 +21,7 @@
       ../../common/optional/vpn.nix
       ../../common/optional/bluetooth.nix
       ../../common/optional/pantalaimon.nix
+      ../../common/optional/zram.nix
       # ../common/optional/postfix.nix
       # ../../common/optional/gamemode.nix
       # ../common/optional/zfs.nix
