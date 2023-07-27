32e6e9339fd006ed444f5df4ac6ca82cb414fb9c
lukasz Dechnik <lukasz@dechnik.net>
2023-07-27 13:58:52 +0200

    lolek.pve ffmpeg

--- hosts/lolek.pve/services/jellyfin.nix
+++ hosts/lolek.pve/services/jellyfin.nix
@@ -1,9 +1,10 @@
-{ lib, config, ... }:
+{ pkgs, lib, config, ... }:
 {
   services.jellyfin = {
     enable = true;
     openFirewall = true;
   };
+  environment.systemPackages = with pkgs; [ jellyfin-ffmpeg ];
   environment.persistence = {
     "/persist".directories = [ "/var/lib/jellyfin" ];
   };
