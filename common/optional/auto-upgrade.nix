3af5abb80117df3b6b34ce547d49587b6aeaca01
lukasz Dechnik <lukasz@dechnik.net>
2023-08-04 18:34:18 +0200

    upstream changes

--- common/optional/auto-upgrade.nix
+++ common/optional/auto-upgrade.nix
@@ -1,4 +1,4 @@
-{ config, inputs, ... }:
+{ pkgs, lib, config, inputs, ... }:
 let
   toplevel = builtins.replaceStrings [".dechnik.net"] [""] config.networking.fqdn;
   hostname = builtins.replaceStrings ["."] ["_"] toplevel;
@@ -14,5 +14,16 @@ in
       "--refresh"
     ];
     flake = "git+https://git.dechnik.net/lukasz/nix-config.git?ref=release-${hostname}#${toplevel}";
+  };
+  # Only run if current config (self) is older than the new one.
+  systemd.services.nixos-upgrade = lib.mkIf config.system.autoUpgrade.enable {
+    serviceConfig.ExecCondition = lib.getExe (
+      pkgs.writeShellScriptBin "check-date" ''
+        lastModified() {
+          nix flake metadata "$1" --refresh --json | ${lib.getExe pkgs.jq} '.lastModified'
+        }
+        test "$(lastModified "${config.system.autoUpgrade.flake}")"  -gt "$(lastModified "self")"
+      ''
+    );
   };
 }
--- home/lukasz/features/games/steam.nix
+++ home/lukasz/features/games/steam.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, config, lib, ... }:
 let
   steam-with-pkgs = pkgs.steam.override {
     extraPkgs = pkgs: with pkgs; [
@@ -12,13 +12,24 @@ let
       stdenv.cc.cc.lib
       libkrb5
       keyutils
+      gamescope
+      mangohud
     ];
   };
+  monitor = lib.head (lib.filter (m: m.isPrimary) config.monitors);
+  steam-session = pkgs.writeTextDir "share/wayland-sessions/steam-sesson.desktop" ''
+    [Desktop Entry]
+    Name=Steam Session
+    Exec=${pkgs.gamescope}/bin/gamescope -W ${toString monitor.width} -H ${toString monitor.height} -O ${monitor.name} -e -- steam -gamepadui
+    Type=Application
+  '';
 in
 {
   home.packages = with pkgs; [
     steam-with-pkgs
     gamescope
+    steam-session
+    mangohud
     protontricks
   ];
   home.persistence = {
