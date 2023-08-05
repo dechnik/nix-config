c021eb01002ba95561991e2a12a32cef4289d76c
lukasz Dechnik <lukasz@dechnik.net>
2023-08-05 13:33:13 +0200

    hyprland hm switch

---
+++ home/lukasz/features/desktop/hyprland/default-old.nix
@@ -0,0 +1,27 @@
+{ inputs, lib, config, pkgs, ... }:
+{
+  imports = [
+    ../common
+    ../common/wayland-wm
+    inputs.hyprland.homeManagerModules.default
+    ./tty-init.nix
+  ];
+
+  home.packages = with pkgs; [
+    inputs.hyprwm-contrib.packages.${system}.grimblast
+    swaybg
+    swayidle
+  ];
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    extraConfig =
+      (import ./monitors.nix {
+        inherit lib;
+        inherit (config) monitors;
+      }) +
+      (import ./config.nix {
+        inherit (config) colorscheme wallpaper;
+      });
+  };
+}
