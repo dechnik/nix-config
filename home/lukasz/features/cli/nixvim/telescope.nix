88be778c7fc01b624d9fe19220a56fd3cf163aeb
lukasz Dechnik <lukasz@dechnik.net>
2023-08-17 21:11:18 +0200

    nixvim tweaking

--- home/lukasz/features/cli/nixvim/telescope.nix
+++ home/lukasz/features/cli/nixvim/telescope.nix
@@ -2,7 +2,7 @@
   programs.nixvim = {
     plugins.telescope.enable = true;
 
-    plugins.telescope.enabledExtensions = [ "ui-select" ];
+    plugins.telescope.enabledExtensions = [ "ui-select" "projects" ];
     plugins.telescope.extensionConfig.ui-select = {};
 
     plugins.telescope.extensions.frecency.enable = true;
@@ -11,12 +11,14 @@
 
     extraPlugins = with pkgs.vimPlugins; [
       telescope-ui-select-nvim
+      project-nvim
     ];
 
     maps.normal = {
       "<leader>pf" = "<cmd>Telescope find_files<cr>";
       "<leader>ps" = "<cmd>Telescope live_grep<cr>";
       "<leader>pb" = "<cmd>Telescope buffers<cr>";
+      "<leader>pp" = "<cmd>Telescope projects<cr>";
       "<leader>ph" = "<cmd>Telescope help_tags<cr>";
 
       "<c-p>" = "<cmd>Telescope find_files<cr>";
