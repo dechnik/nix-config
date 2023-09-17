45faf960b237836c1fc276a8ca903e228f10dd2f
lukasz Dechnik <lukasz@dechnik.net>
2023-09-17 11:20:02 +0200

    pycharm

--- home/lukasz/features/desktop/optional/videoedit.nix
+++ home/lukasz/features/desktop/optional/videoedit.nix
@@ -1,6 +1,22 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
+  home.packages = with pkgs; let
+    resolve-desktop = makeDesktopItem {
+      name = "davinci-resolve";
+      desktopName = "Davinci Resolve";
+      genericName = "Davinci Resolve";
+      keywords = [ "Video" "Editor" ];
+      comment = "Edit video";
+      type = "Application";
+      terminal = false;
+      startupWMClass = "resolve";
+      exec = "davinci-resolve";
+      mimeTypes = [
+        "application/x-resolveproj"
+      ];
+    };
+  in [
     davinci-resolve
+    resolve-desktop
   ];
 }
