dd199c90397c6e3d810ca265ab013a57960567d5
lukasz Dechnik <lukasz@dechnik.net>
2023-10-07 15:40:26 +0200

    nvim flake update

--- home/lukasz/features/cli/neovim.nix
+++ home/lukasz/features/cli/neovim.nix
@@ -1,24 +1,29 @@
-{ inputs, pkgs, ... }:
-let
-  pythonEnv = pkgs.python3.withPackages (ps: with ps; [
-    python-lsp-server
-    pyls-isort
-    pytest
-    black
-    nose
-    mypy
-    pylama
-    flake8
-    tldextract # required by qute-pass
-  ] ++ python-lsp-server.optional-dependencies.all);
-in
 {
+  inputs,
+  pkgs,
+  ...
+}: let
+  pythonEnv = pkgs.python3.withPackages (ps:
+    with ps;
+      [
+        python-lsp-server
+        pyls-isort
+        pytest
+        black
+        nose
+        mypy
+        pylama
+        flake8
+        tldextract # required by qute-pass
+      ]
+      ++ python-lsp-server.optional-dependencies.all);
+in {
   xdg.desktopEntries."nvim" = {
     name = "NeoVim";
     comment = "Edit text files";
     icon = "nvim";
     exec = "${pkgs.kitty}/bin/kitty -1 -e ${pkgs.neovim}/bin/nvim %F";
-    categories = [ "TerminalEmulator" ];
+    categories = ["TerminalEmulator"];
     terminal = false;
     mimeType = [
       "text/english"
@@ -45,12 +50,12 @@ in
     deadnix
     pythonEnv
     binutils # native-comp needs 'as', provided by this
-    (ripgrep.override { withPCRE2 = true; })
+    (ripgrep.override {withPCRE2 = true;})
     gnutls # for TLS connectivity
     fd # faster projectile indexing
     imagemagick # for image-dired
     zstd # for undo-fu-session/undo-tree compression
-    (aspellWithDicts (ds: with ds; [ en en-computers en-science ]))
+    (aspellWithDicts (ds: with ds; [en en-computers en-science]))
     editorconfig-core-c # per-project style config
     sqlite
     rnix-lsp
@@ -66,6 +71,7 @@ in
     nodePackages.yaml-language-server
     nodePackages_latest.pyright
     nodePackages.write-good
+    sumneko-lua-language-server
     # nodejs-18_x
     # nodePackages.typescript-language-server
   ];
