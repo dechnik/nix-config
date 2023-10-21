22e618e64517eeb04d58b50aa6724a174c049648
lukasz Dechnik <lukasz@dechnik.net>
2023-10-21 21:34:51 +0200

    tola.pve network setup

--- hosts/tola.pve/default.nix
+++ hosts/tola.pve/default.nix
@@ -7,6 +7,10 @@
 
       ../../common/optional/qemu-vm.nix
       ../../common/optional/nix-gc.nix
+      ../../common/optional/consul.nix
+      ../../common/optional/promtail.nix
+      ../../common/optional/node-exporter.nix
+      ../../common/optional/systemd-exporter.nix
       ../../common/global
       ../../common/users/lukasz
       inputs.disko.nixosModules.disko
@@ -16,6 +20,8 @@
     kernelPackages = pkgs.linuxKernel.packages.linux_zen;
   };
 
+  my.lan = "ens19";
+
   networking = {
     hostName = "tola"; # Define your hostname.
     domain = "pve.dechnik.net";
@@ -23,6 +29,24 @@
     interfaces."ens18" = {
       useDHCP = true;
     };
+    interfaces."${config.my.lan}" = {
+      useDHCP = false;
+      ipv4.addresses = [
+        {
+          address = "10.60.0.3";
+          prefixLength = 24;
+        }
+      ];
+      ipv4.routes = [
+        {
+          address = "10.60.0.1";
+          prefixLength = 32;
+        }
+      ];
+    };
   };
   system.stateVersion = "23.11"; # Did you read the comment?
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 80;
+  };
 }
