da9057ff0ef5a6fe4405d2d9ddb86164a8b07260
lukasz Dechnik <lukasz@dechnik.net>
2024-01-09 20:07:45 +0100

    lutris persistence removal

--- home/lukasz/features/games/lutris.nix
+++ home/lukasz/features/games/lutris.nix
@@ -8,28 +8,28 @@
     })
   ];
 
-  home.persistence = {
-    "/persist/home/lukasz" = {
-      allowOther = true;
-      directories = [
-        {
-          # Use symlink, as games may be IO-heavy
-          directory = "Games/Lutris";
-          method = "symlink";
-        }
-        {
-          # Use symlink, as games may be IO-heavy
-          directory = "Games/epic-games-store";
-          method = "symlink";
-        }
-        {
-          # Use symlink, as games may be IO-heavy
-          directory = "Games/battlenet";
-          method = "symlink";
-        }
-        ".config/lutris"
-        ".local/share/lutris"
-      ];
-    };
-  };
+  # home.persistence = {
+  #   "/persist/home/lukasz" = {
+  #     allowOther = true;
+  #     directories = [
+  #       {
+  #         # Use symlink, as games may be IO-heavy
+  #         directory = "Games/Lutris";
+  #         method = "symlink";
+  #       }
+  #       {
+  #         # Use symlink, as games may be IO-heavy
+  #         directory = "Games/epic-games-store";
+  #         method = "symlink";
+  #       }
+  #       {
+  #         # Use symlink, as games may be IO-heavy
+  #         directory = "Games/battlenet";
+  #         method = "symlink";
+  #       }
+  #       ".config/lutris"
+  #       ".local/share/lutris"
+  #     ];
+  #   };
+  # };
 }
