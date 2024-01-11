bfb64000daa718d85c3c0333c02c713fadf0f45d
lukasz Dechnik <lukasz@dechnik.net>
2024-01-11 08:42:31 +0100

    obsidian allow electron 25

--- home/lukasz/global/default.nix
+++ home/lukasz/global/default.nix
@@ -15,6 +15,8 @@ in
     config = {
       allowUnfree = true;
       allowUnfreePredicate = (_: true);
+      # TODO remove after obsidian changes electron version
+      permittedInsecurePackages = pkgs.lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0";
     };
   };
 
