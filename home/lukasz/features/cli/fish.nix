88450b45ba96beafceae354c326f526c4ccf585e
lukasz Dechnik <lukasz@dechnik.net>
2024-02-08 15:38:13 +0100

    fish kitty fix

--- home/lukasz/features/cli/fish.nix
+++ home/lukasz/features/cli/fish.nix
@@ -88,11 +88,12 @@ in
       ssh = mkIf hasShellColor ''
         ${shellcolor} disable $fish_pid
         # Check if kitty is available
-        if set -q KITTY_PID && set -q KITTY_WINDOW_ID && type -q -f kitty
-          kitty +kitten ssh $argv
-        else
-          command ssh $argv
-        end
+        # if set -q KITTY_PID && set -q KITTY_WINDOW_ID && type -q -f kitty
+        #   kitty +kitten ssh $argv
+        # else
+        #   command ssh $argv
+        # end
+        command ssh $argv
         ${shellcolor} enable $fish_pid
         ${shellcolor} apply $fish_pid
       '';
