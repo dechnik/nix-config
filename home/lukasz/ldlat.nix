366d23de127301fdd2063f949a819aa583b433cf
lukasz Dechnik <lukasz@dechnik.net>
2024-03-13 11:38:32 +0100

    wallpaper fix

--- home/lukasz/ldlat.nix
+++ home/lukasz/ldlat.nix
@@ -65,7 +65,7 @@
 
   wallpaper = builtins.fetchurl rec {
     name = "wallpaper-${sha256}.png";
-    url = "https://git.sr.ht/~lukasz/dotfiles/blob/master/home/graphical/files/config/wallpaper.png";
+    url = "https://raw.githubusercontent.com/dechnik/nix-config/master/home/lukasz/features/desktop/wall.png";
     sha256 = "37bfdbb9cd427e2c6ebee1de458f6a96704d47962220332c5b7e2e316fef77e0";
   };
 }
