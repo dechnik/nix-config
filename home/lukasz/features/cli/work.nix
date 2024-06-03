f4c6d8eb7be4c1ac8083841faa745dd53b2317f9
lukasz Dechnik <lukasz@dechnik.net>
2024-06-03 07:17:16 +0200

    azure cli

--- home/lukasz/features/cli/work.nix
+++ home/lukasz/features/cli/work.nix
@@ -4,9 +4,11 @@
   home.packages = with pkgs; [
     awscli
     # aws-sam-cli
-    # azure-cli
+    azure-cli
+    # azure-functions-core-tools
     terraform
   ];
+  home.sessionVariables.FUNCTIONS_CORE_TOOLS_TELEMETRY_OPTOUT = 1;
   home.sessionVariables = {
     AWS_CONFIG_FILE = "$HOME/.config/aws/config";
     AWS_SHARED_CREDENTIALS_FILE = "$HOME/.config/aws/credentials";
