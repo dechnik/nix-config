28d95df33ffdea76657d90182c9ebeac39763a6f
lukasz Dechnik <lukasz@dechnik.net>
2024-06-03 11:11:18 +0200

    plymouth spinner removal

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,6 @@
   pass-wofi = pkgs.callPackage ./pass-wofi { };
   primary-xwayland = pkgs.callPackage ./primary-xwayland { };
   wl-mirror-pick = pkgs.callPackage ./wl-mirror-pick { };
-  plymouth-spinner-monochrome = pkgs.callPackage ./plymouth-spinner-monochrome { };
   xpo = pkgs.callPackage ./xpo { };
   spiceto = pkgs.callPackage ./spiceto { };
   cyrus-sasl-xoauth2 = pkgs.callPackage ./cyrus-sasl-xoauth2 { };
