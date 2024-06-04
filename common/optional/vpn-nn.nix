4848fb1cb1534cdbcef9ec2c6bf0f7501776c63c
lukasz Dechnik <lukasz@dechnik.net>
2024-06-04 08:15:36 +0200

    nix flake update

--- common/optional/vpn-nn.nix
+++ common/optional/vpn-nn.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    networkmanagerapplet
+  ];
   networking = {
     networkmanager.plugins = with pkgs; [
       networkmanager-l2tp
