d3135ff63dbc875c2f99e7c8c822fcfc18cb369b
lukasz Dechnik <lukasz@dechnik.net>
2024-08-27 09:27:04 +0200

    home: swayidle remove grace

--- home/lukasz/features/desktop/common/wayland-wm/swayidle.nix
+++ home/lukasz/features/desktop/common/wayland-wm/swayidle.nix
@@ -41,7 +41,7 @@ in
       [
         {
           timeout = lockTime;
-          command = "${swaylock} -i ${config.wallpaper} --daemonize --grace 15";
+          command = "${swaylock} -i ${config.wallpaper} --daemonize";
         }
       ]
       ++
