662083a82f2d17f369e56663380de2d93af34f83
lukasz Dechnik <lukasz@dechnik.net>
2024-09-03 20:27:09 +0200

    home: azure telemetry off

--- home/lukasz/features/cli/work.nix
+++ home/lukasz/features/cli/work.nix
@@ -11,6 +11,7 @@
     terraform
   ];
   home.sessionVariables.FUNCTIONS_CORE_TOOLS_TELEMETRY_OPTOUT = 1;
+  home.sessionVariables.AZURE_DEV_COLLECT_TELEMETRY = "no";
   home.sessionVariables = {
     AWS_CONFIG_FILE = "$HOME/.config/aws/config";
     AWS_SHARED_CREDENTIALS_FILE = "$HOME/.config/aws/credentials";
